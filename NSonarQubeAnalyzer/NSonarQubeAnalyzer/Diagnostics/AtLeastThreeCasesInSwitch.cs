5295b19b1d7978864ca1a73d9472fe02d6067f68
Peter Gill <peter@majorsilence.com>
2015-03-14 19:06:39 -0230

    Switch AtLeastThreeCasesInSwitch.cs to vb

--- NSonarQubeAnalyzer/NSonarQubeAnalyzer/Diagnostics/AtLeastThreeCasesInSwitch.cs
+++ NSonarQubeAnalyzer/NSonarQubeAnalyzer/Diagnostics/AtLeastThreeCasesInSwitch.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Microsoft.CodeAnalysis.VisualBasic;
 using Microsoft.CodeAnalysis.Text;
 using System.Collections.Immutable;
 using System.Threading;
@@ -18,8 +18,8 @@ namespace NSonarQubeAnalyzer
     public class AtLeastThreeCasesInSwitch : DiagnosticAnalyzer
     {
         internal const string DiagnosticId = "S1301";
-        internal const string Description = "\"switch\" statements should have at least 3 \"case\" clauses";
-        internal const string MessageFormat = "Replace this \"switch\" statement with \"if\" statements to increase readability.";
+        internal const string Description = "\"Select Case\" statements should have at least 3 \"Case\" clauses";
+        internal const string MessageFormat = "Replace this \"Select Case\" statement with \"if\" statements to increase readability.";
         internal const string Category = "SonarQube";
         internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
 
@@ -29,21 +29,24 @@ namespace NSonarQubeAnalyzer
 
         public override void Initialize(AnalysisContext context)
         {
+         
             context.RegisterSyntaxNodeAction(
                 c =>
                 {
-                    SwitchStatementSyntax switchNode = (SwitchStatementSyntax)c.Node;
+                    SelectStatementSyntax switchNode = (SelectStatementSyntax)c.Node;
                     if (!HasAtLeastThreeLabels(switchNode))
                     {
                         c.ReportDiagnostic(Diagnostic.Create(Rule, c.Node.GetLocation()));
                     }
                 },
-                SyntaxKind.SwitchStatement);
+                SyntaxKind.SelectStatement);
         }
 
-        private static bool HasAtLeastThreeLabels(SwitchStatementSyntax node)
+        private static bool HasAtLeastThreeLabels(SelectStatementSyntax node)
         {
-            return node.Sections.Sum(section => section.Labels.Count) >= 3;
+            // TODO: How do I check the number of case statements in vb?   
+            return false;
+            //return node.Sections.Sum(section => section.Labels.Count) >= 3;
         }
     }
 }
